b6105faf67857a3f870d76eea53aba507acefe57
mykha <efimenko.mykhailo@gmail.com>
2020-04-20 11:24:10 +0300

    v.1 engish version

--- app10_rsp.rb
+++ app10_rsp.rb
@@ -1,46 +1,46 @@
 # encoding : cp866
-#счет игры (изначально 0:0)
+#game score (initially 0:0)
 score = {:user=>0, :comp=>0}
-# в массиве сохранены отношения, у кого меньше индекс, тот и победил
+# relations are kept in the array, whose index is lower - he won
 arr_range = [[:rock, :scissors], [:scissors, :paper], [:paper, :rock]]
-# варианты ответов
+# varied answers
 arr = [:rock, :scissors, :paper]
 user_range_index = nil
 winplace = nil
 loop do
-	puts "Общий счет : Вы(#{score[:user]}):Компьютер(#{score[:comp]})"
-	print "Сделайте ваш выбор R(rock), S(scissors), P(paper)"
+	puts "Total score : YOU(#{score[:user]}):COMPUTER(#{score[:comp]})"
+	print "Make your choice R(rock), S(scissors), P(paper)"
 	user_choice_str = gets.strip.downcase[0]
 	comp_choice = arr[rand(0..2)]
 	if "rsp".include?(user_choice_str) 
 		user_choice = arr["rsp".index(user_choice_str)]
-		puts "Ваш выбор #{user_choice.to_s.capitalize}"
-		puts "Выбор компьютера #{comp_choice.to_s.capitalize}"
+		puts "Your choice is #{user_choice.to_s.capitalize}"
+		puts "Computer choice is #{comp_choice.to_s.capitalize}"
 
 		if user_choice == comp_choice 
-			puts "Ничья !"
+			puts "Draw !"
 		else
-			#в каждом подмассивчике ищем ответ пользователя и компьютера
+			#in each subarray we look for the response of the user and the computer
 			arr_range.each_with_index do |item, idx|
 				user_range_index = item.index(user_choice)
 				comp_range_index = item.index(comp_choice)
-				#если нашли подмассивчик, где есть оба ответа, прекращаем поиск
+				#if we find a subarray where there are both answers - stop the search
 				if (user_range_index != nil) && (comp_range_index != nil) 
 					winplace = idx
 					break
 				end	
 			end
-			#если индекс равен 0, то пользователь выиграл
+			#if the index is 0 then the user won
 			if user_range_index==0	
-				puts "Вы выиграли !"
+				puts "You won !"
 				score[:user]+=1
 			else
-				puts "Выиграл компьютер :("
+				puts "Computer won :("
 				score[:comp]+=1
 			end
 			puts arr_range[winplace].index(user_choice) < arr_range[winplace].index(comp_choice)
 		end
 	else
-		puts "Непонятный выбор. Нажмите одну из клавиш - r , s или p"
+		puts "Wrong choice. Please, press one of the keys - r , s or p"
 	end
 end
